f7db9ff08eeade26369acfd24646cc3764e8fdf9
Vitor Moreira <119359869+vitor-dev-linux@users.noreply.github.com>
2023-05-01 17:27:58 -0300

    Incrementando fucntionPalindromo

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Insertion function palindromo
+def functionPalindromo(palavra):
+    comparador = palavra
+    if palavra == palavra[::-1]:
+        return "E palindromo"
+    else:
+        return "Não!!! e palindromo"
+    
 # imports locale
 from Fatorial import Fatorial
 
@@ -10,3 +18,5 @@ x = int(x)
 fatorial = Fatorial()
 print("Fim do timer, obrigado por testar")
 print("O fatorial deste numero e ",fatorial.fatorial(x))
+palavra = input("Digite uma palavra para verificar se ela é palindroma ou não: ")
+print(functionPalindromo(palavra))
