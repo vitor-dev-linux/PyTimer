98c5af555fed36b5a7446228938dfcbde90ea86e
OsvaldoRodrigues <osvaldosanca6@gmail.com>
2023-05-01 17:29:37 -0300

    Timer tá funcionando

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # imports locale
 from Fatorial import Fatorial
+from Timer import Timer 
+
 
 x = input("Quanto tempo quer contabilizar?: ")
 x = int(x)
 # Insertion function timer
 # 
-# 
+Timer.countdown(x)  
 #  Insertion function fatorial
 fatorial = Fatorial()
 print("Fim do timer, obrigado por testar")
